c96d570f6b110ba358befa3126f28d113d6f496e
Josh W <josher19@users.sf.net>
2024-02-22 19:07:15 -0800

    Update ARN

--- order-creation/functions/hello-world/app.ts
+++ order-creation/functions/hello-world/app.ts
@@ -10,11 +10,59 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
+function getSalesforceData(event: APIGatewayProxyEvent) {
+    // const salesforce = require('node-salesforce');
+    // TODO: Do something with the event
+    const salesforceData = Object.assign({
+        "email": "jon.sample@example.com",
+        "firstName": "Jon",
+        "lastName": "Sample",
+        "publicId": "e7ng48d",
+        "guid": "e0a0d70d-1234-8888-bb7b-db925e7da417",
+        "stripe_customerId": "cus_XXXXXXXXXXXXXX",
+        "shippingStreet": "123 Main St",
+        "shippingCity": "Portland",
+        "shippingState": "OR",
+        "shippingCountry": "US",
+        "shippingPostalCode": "97214",
+        "doNotDeliverPerks": false,
+        "doNotDeliver": false,
+        "nationalId": null,
+        "phoneNumber": null,
+        "langSourceContact": "eng",
+        "paymentMethod": "Stripe",
+        "timestamp": "2024-01-10",
+        "total": 58.99,
+        "donationComments": null,
+        "videoId": null,
+        "transactionId": "ch_ZZX1cR4ig8703iAj0eMZZZZZ",
+        "purchaseLines": [
+            {
+                "productId": "01t1C000005jrhkQAA",
+                "quantity": 1,
+                "total": 50
+            },
+            {
+                "productId": "01t1C000005jrhlQAA",
+                "quantity": 1,
+                "total": 8.99
+            }
+        ]
+    }, { event });
+
+    return salesforceData;
+}
+
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    console.log('Event: ', typeof event, event);
+    if (typeof event.body === 'string' && event.body[0] === '{') {
+        event.body = JSON.parse(event.body)
+        console.log('body', event.body);
+    }
+    const salesforce = getSalesforceData(event);
+
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            message: 'hello world',
-        }),
+        body: JSON.stringify({ salesforce }),
     };
 };
